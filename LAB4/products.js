d79c39a39156e6f1697caf9e2a57877039bde1d8
2019181013 <128265810+2019181013@users.noreply.github.com>
2023-05-22 01:18:12 +0900

    Delete LAB4 directory

--- LAB4/products.js
+++
@@ -1,9 +0,0 @@
-function show_products() {
-    fetch("https://2019181013.github.io/HomeworkRepository/LAB4/product.json")
-    .then(function(response){
-        return response.json();
-    })
-    .then((items => {
-        console.log(items);
-    }));
-}
